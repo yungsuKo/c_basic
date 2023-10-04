53d7da17d8795df8ea038d5cf7ca735e2dbe9ec0
Tom Ko <yungsu2391@gmail.com>
2023-10-05 00:41:41 +0900

    file 입력

---
+++ c_basic/file1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    fp = fopen("a.txt", "w");
+
+    if(fp == NULL){
+        printf("write Error! \n");
+        return 0;
+    }
+    fputs("Hello World! \n", fp);
+    fclose(fp);
+
+    return 0;
+}
